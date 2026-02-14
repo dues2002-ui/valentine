11f8669dcc0e2085d5134fd0e5e67979943a7dc6
dues2002-ui <dues2002@gmail.com>
2026-02-14 12:18:36 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,10 +22,10 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Хамтдаа гадуур гархуу ?",                                    // First interaction
+            text: "Хамтдаа гадуур гарж тэмдэглхуу Хөөрхнө ?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "Тэгийлдаа ❤️"           // Secret hover message
         },
         second: {
             text: "How much do you love me?",                          // For the love meter
